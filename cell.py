23007d1850d8424c655f3db25921aaa4988e6785
Renat Shabanov <multizloy@gmail.com>
2024-09-11 10:02:38 +0300

    draw method

--- cell.py
+++ cell.py
@@ -9,3 +9,36 @@ class Cell:
         self.thickness = thickness
         self.walls = {"top": True, "bottom": True, "left": True, "right": True}
         self.visited = False
+
+    # draw grid cell walls
+    def draw(self, sc: pygame.Surface, tile: int):
+        x = self.x * tile
+        y = self.y * tile
+
+        if self.walls["top"]:
+            pygame.draw.line(
+                sc, pygame.Color("darkgreen"), (x, y), (x + tile, y), self.thickness
+            )
+
+        if self.walls["bottom"]:
+            pygame.draw.line(
+                sc,
+                pygame.Color("darkgreen"),
+                (x, y + tile),
+                (x + tile, y + tile),
+                self.thickness,
+            )
+
+        if self.walls["left"]:
+            pygame.draw.line(
+                sc, pygame.Color("darkgreen"), (x, y), (x, y + tile), self.thickness
+            )
+
+        if self.walls["right"]:
+            pygame.draw.line(
+                sc,
+                pygame.Color("darkgreen"),
+                (x + tile, y),
+                (x + tile, y + tile),
+                self.thickness,
+            )
