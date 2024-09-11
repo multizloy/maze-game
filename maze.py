240330af9cf03905722d65ad97492523f8704511
Renat Shabanov <multizloy@gmail.com>
2024-09-11 10:28:57 +0300

    generate maze method

--- maze.py
+++ maze.py
@@ -30,3 +30,24 @@ class Maze:
         elif dy == -1:
             current.walls["top"] = False
             next.walls["bottom"] = False
+
+    # generate maze
+    def generate_maze(self):
+        current_cell = self.grid_cells[0]
+        array = []
+        break_count = 1
+        while break_count != len(self.grid_cells):
+            current_cell.visited = True
+            next_cell = current_cell.check_neighbors(
+                self.cols, self.rows, self.grid_cells
+            )
+            if next_cell:
+                next_cell.visited = True
+                break_count += 1
+                array.append(current_cell)
+                self.remove_walls(current_cell, next_cell)
+                current_cell = next_cell
+            elif array:
+                current_cell = array.pop()
+
+        return self.grid_cells
