62e36295ccea03d230883089c845632f7300aab3
Renat Shabanov <multizloy@gmail.com>
2024-09-11 10:31:39 +0300

    player.py, class Player , init method

---
+++ player.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+class Player:
+    def __init__(self, x: int, y: int):
+        self.x = int(x)
+        self.y = int(y)
+        self.player_size = 10
+        self.rect = pygame.Rect(self.x, self.y, self.player_size, self.player_size)
+        self.color = (250, 120, 60)
+        self.velX = 0
+        self.velY = 0
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+        self.speed = 4
