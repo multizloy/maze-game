f840d21744c3d715aa62362e125c697116c7b42f
Renat Shabanov <multizloy@gmail.com>
2024-09-11 10:23:41 +0300

    remove wall method

--- maze.py
+++ maze.py
@@ -13,3 +13,20 @@ class Maze:
             for row in range(self.rows)
             for col in range(self.cols)
         ]
+
+    # carve grid cell walls
+    def remove_walls(self, current: Cell, next: Cell):
+        dx = current.x - next.x
+        if dx == 1:
+            current.walls["left"] = False
+            next.walls["right"] = False
+        elif dx == -1:
+            current.walls["right"] = False
+            next.walls["left"] = False
+        dy = current.y - next.y
+        if dy == 1:
+            current.walls["top"] = False
+            next.walls["bottom"] = False
+        elif dy == -1:
+            current.walls["top"] = False
+            next.walls["bottom"] = False
